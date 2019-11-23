708a4315737182a0eb2e50c3c4b9c343f6cb3709
persona1346 <bosoeva2001@mail.ru>
2019-11-24 00:37:23 +0300

    сделала консолькуб все работает

--- homework03/life-console.py
+++ homework03/life-console.py
@@ -11,13 +11,43 @@ class Console(UI):
 
     def draw_borders(self, screen) -> None:
         """ Отобразить рамку. """
-        pass
+        for i in range(life.rows+1):
+            screen.addstr(i,0,'|')
+            screen.addstr(i,life.cols+1,'|')
+        for j in range(life.cols+1):
+            screen.addstr(0,j,'-')
+            screen.addstr(life.rows+1, j,'-')
+        screen.addstr(0, 0, '+')
+        screen.addstr(0, life.cols+1, '+')
+        screen.addstr(life.rows+1, 0, '+')
+        screen.addstr(life.rows+1, life.cols+1, '+')
 
     def draw_grid(self, screen) -> None:
         """ Отобразить состояние клеток. """
-        pass
+        for i in range(life.rows):
+            for j in range(life.cols):
+                if life.curr_generation[i][j]:
+                    screen.addstr(i+1, j+1, "*")
+                else:
+                    screen.addstr(i+1, j+1, ' ')
+
 
     def run(self) -> None:
         screen = curses.initscr()
-        # PUT YOUR CODE HERE
+        curses.curs_set(0)
+
+        self.draw_borders(screen)
+        self.draw_grid(screen)
+
+        running = True
+
+        while running:
+            while life.is_changing and not life.is_max_generations_exceeded:
+                life.step()
+                self.draw_grid(screen)
+                screen.refresh()
+                time.sleep(0.5)
+            else:
+                running = False
+
         curses.endwin()
